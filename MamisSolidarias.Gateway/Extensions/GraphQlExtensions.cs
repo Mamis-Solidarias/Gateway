15159c6719f55de87df688269db60f3516c34f91
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-05 16:14:57 +0000

    Fix graphql gateway (#11)
    * [Fix] Removed yaml settings
    
    * [Fix] fixed graphql gateway

--- MamisSolidarias.Gateway/Extensions/GraphQlExtensions.cs
+++ MamisSolidarias.Gateway/Extensions/GraphQlExtensions.cs
@@ -6,14 +6,12 @@ internal static class GraphQlExtensions
 {
     public static void AddGraphQl(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddGraphQlHttpClient(Services.Users, configuration["GraphQl:Users:Url"]);
         services.AddGraphQlHttpClient(Services.Beneficiaries, configuration["GraphQl:Beneficiaries:Url"]);
         services.AddGraphQlHttpClient(Services.Donors, configuration["GraphQl:Donors:Url"]);
         
         services.AddGraphQLServer()
-            .AddRemoteSchema($"{Services.Users}-gql")
-            .AddRemoteSchema($"{Services.Beneficiaries}-gql")
-            .AddRemoteSchema($"{Services.Donors}-gql")
+            .AddRemoteSchema($"{Services.Beneficiaries}gql")
+            .AddRemoteSchema($"{Services.Donors}gql")
             ;
     }
 
