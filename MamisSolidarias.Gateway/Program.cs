0f897335e164939cc2f65b5571262f35e5dfac5b
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-24 23:04:24 -0300

    [Fix] Persist secure keys over container restarts (#42)

--- MamisSolidarias.Gateway/Program.cs
+++ MamisSolidarias.Gateway/Program.cs
@@ -1,8 +1,12 @@
 using MamisSolidarias.Gateway.Extensions;
 using MamisSolidarias.HttpClient.Users;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDataProtection(builder.Configuration);
 builder.Services.AddAuthEndpoints(builder.Configuration,builder.Environment);
 builder.Services.AddAuth(builder.Configuration);
 builder.Services.AddYarp(builder.Configuration);
