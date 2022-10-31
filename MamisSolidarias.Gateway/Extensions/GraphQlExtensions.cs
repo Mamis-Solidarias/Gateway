bfe5a7cfdcaec69a0dbecbe19d6509f371b1abe1
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-31 20:17:41 -0300

    [Feature] Adopt GraphQl schema push with Redis cache (#52)

--- MamisSolidarias.Gateway/Extensions/GraphQlExtensions.cs
+++ MamisSolidarias.Gateway/Extensions/GraphQlExtensions.cs
@@ -1,6 +1,7 @@
 using HotChocolate.Diagnostics;
 using HotChocolate.Execution.Configuration;
 using MamisSolidarias.Utils.Security;
+using StackExchange.Redis;
 
 namespace MamisSolidarias.Gateway.Extensions;
 
@@ -13,11 +14,11 @@ internal static class GraphQlExtensions
         services.AddGraphQlHttpClient(Services.Users, configuration["GraphQl:Users:Url"]);
         services.AddGraphQlHttpClient(Services.Campaigns, configuration["GraphQl:Campaigns:Url"]);
 
+        var redisConnectionString = $"{configuration["Redis:Host"]}:{configuration["Redis:Port"]}";
+        services.AddSingleton(ConnectionMultiplexer.Connect(redisConnectionString));
+
         services.AddGraphQLServer()
-            .AddGraphQlSchema(services, Services.Beneficiaries, configuration["GraphQl:Beneficiaries:Url"])
-            .AddGraphQlSchema(services, Services.Donors, configuration["GraphQl:Donors:Url"])
-            .AddGraphQlSchema(services, Services.Users, configuration["GraphQl:Users:Url"])
-            .AddGraphQlSchema(services, Services.Campaigns, configuration["GraphQl:Campaigns:Url"])
+            .AddRemoteSchemasFromRedis(configuration["GraphQl:GlobalSchemaName"],t=> t.GetRequiredService<ConnectionMultiplexer>())
             .AddInstrumentation(t =>
             {
                 t.Scopes = ActivityScopes.All;
@@ -25,20 +26,6 @@ internal static class GraphQlExtensions
                 t.RequestDetails = RequestDetails.All;
                 t.IncludeDataLoaderKeys = true;
             });
-    }
-
-    private static IRequestExecutorBuilder AddGraphQlSchema(this IRequestExecutorBuilder graphql, IServiceCollection services , Services name, string? url)
-    {
-        if ( string.IsNullOrEmpty(url))
-        {
-            Console.WriteLine($"Skipping {name} because it is not configured");
-            return graphql;
-        }
-        
-        Console.WriteLine($"Adding GraphQL Schema for {name} at {url}");
-
-        services.AddGraphQlHttpClient(name, url);
-        return graphql.AddRemoteSchema($"{name}gql");
     }
 
     public static void UseGraphQl(this WebApplication app)
