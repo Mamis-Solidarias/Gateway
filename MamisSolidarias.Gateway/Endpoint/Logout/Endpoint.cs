c1a68af54543356653debc26e82ad49f39a8edbf
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-02-06 11:04:11 -0300

    [Fix] Don't remove roles on logout

--- MamisSolidarias.Gateway/Endpoint/Logout/Endpoint.cs
+++ MamisSolidarias.Gateway/Endpoint/Logout/Endpoint.cs
@@ -7,11 +7,6 @@ namespace MamisSolidarias.Gateway.Endpoint.Logout;
 
 internal sealed class Endpoint : EndpointWithoutRequest
 {
-	private readonly IRolesCache _rolesCache;
-	public Endpoint(IRolesCache rolesCache)
-	{
-		_rolesCache = rolesCache;
-	}
 	public override void Configure()
     {
         Post("/logout");
@@ -19,12 +14,7 @@ internal sealed class Endpoint : EndpointWithoutRequest
 
     public override async Task HandleAsync(CancellationToken ct)
     {
-	    var userId = User.GetUserId();
-
-	    if (userId is not null)
-		    await _rolesCache.ClearRoles(userId.Value);
-	    
-        await HttpContext.SignOutAsync();
+	    await HttpContext.SignOutAsync();
         await SendOkAsync(cancellation: ct);
     }
 }
